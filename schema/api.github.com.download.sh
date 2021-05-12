e2f2511339c0bbc55a5e2a06fd994bfd7d16376a
anatawa12 <anatawa12@icloud.com>
2021-05-12 18:47:27 +0900

    rename files for github api schemas

--- schema/api.github.com.download.sh
+++ schema/api.github.com.download.sh
@@ -2,6 +2,6 @@
 COMMIT="${1:-main}"
 URL_BASE="https://github.com/github/rest-api-description/raw/$COMMIT/descriptions/api.github.com"
 
-echo "$COMMIT" > .commit
+echo "$COMMIT" > api.github.com.commit.txt
 curl -L "$URL_BASE/api.github.com.yaml" > api.github.com.yaml
 curl -L "$URL_BASE/api.github.com.json" > api.github.com.json
