9949993a32759cc018a29eefb99510cb13561897
anatawa12 <anatawa12@icloud.com>
2021-05-14 20:18:21 +0900

    remove yaml

--- schema/api.github.com.download.sh
+++ schema/api.github.com.download.sh
@@ -3,5 +3,4 @@ COMMIT="${1:-main}"
 URL_BASE="https://github.com/github/rest-api-description/raw/$COMMIT/descriptions/api.github.com"
 
 echo "$COMMIT" > api.github.com.commit.txt
-curl -L "$URL_BASE/api.github.com.yaml" > api.github.com.yaml
 curl -L "$URL_BASE/api.github.com.json" > api.github.com.json
