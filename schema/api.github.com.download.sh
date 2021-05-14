b8a013c614148ed0d8a715a0d9987045222925ab
anatawa12 <anatawa12@icloud.com>
2021-05-14 20:22:32 +0900

    add patch system for github schema

--- schema/api.github.com.download.sh
+++ schema/api.github.com.download.sh
@@ -4,3 +4,7 @@ URL_BASE="https://github.com/github/rest-api-description/raw/$COMMIT/description
 
 echo "$COMMIT" > api.github.com.commit.txt
 curl -L "$URL_BASE/api.github.com.json" > api.github.com.json
+patch api.github.com.patched.json < api.github.com.json.patch
+diff -u api.github.com.json api.github.com.patched.json \
+  | perl -pe 's/^((\+\+\+|---)[^\t]*).*$/$1/' \
+  > api.github.com.json.patch
